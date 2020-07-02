e0f5c06c4779388edfa92176aee2571fde072d3f
unknown <pawel.pawlaszczyk@gmail.com>
2020-07-02 21:04:10 +0200

    Chapter 2

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: HomeControler
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Gretting = hour < 17 ? "Dzień dobry" : "Dobry wieczór";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                // do zrobienia: wyslij zawartosc guest respone do organizatora  przyjecia
+                return View("thanks", guestResponse);
+            }
+            else
+            { //błąd kontorli porpawnosci. Ponownie wyswietla formularz wprowadzenia danycgh
+                return View();
+            }
+        }
+    }
+}
